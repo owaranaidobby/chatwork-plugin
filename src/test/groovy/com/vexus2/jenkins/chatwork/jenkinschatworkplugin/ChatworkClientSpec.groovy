e354406a3aa6735e5e829cea3b5acb0534783204
sue445 <sue445@sue445.net>
2015-02-28 02:37:24 +0900

    Refactor: junit test -> spock test

---
+++ src/test/groovy/com/vexus2/jenkins/chatwork/jenkinschatworkplugin/ChatworkClientSpec.groovy
@@ -0,0 +1,40 @@
+package com.vexus2.jenkins.chatwork.jenkinschatworkplugin
+
+import co.freeside.betamax.Betamax
+import co.freeside.betamax.MatchRule
+import co.freeside.betamax.Recorder
+import co.freeside.betamax.TapeMode
+import org.junit.Rule
+import spock.lang.Ignore
+import spock.lang.Specification
+
+class ChatworkClientSpec extends Specification {
+  static final String TAPE_NAME = "ChatWork_v1_POST_rooms_messages"
+
+  ChatworkClient client
+
+  @Rule
+  Recorder recorder = new Recorder()
+
+  def setup(){
+    String apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    String proxySv = "NOPROXY"
+    String proxyPort = "80"
+    String channelId = "00000000"
+    client = new ChatworkClient(apiKey, proxySv, proxyPort, channelId)
+  }
+
+  @Betamax(tape=ChatworkClientSpec.TAPE_NAME, mode = TapeMode.READ_ONLY, match = [MatchRule.host, MatchRule.path])
+  def "sendMessage should send message"(){
+    expect:
+    client.sendMessage("testMessage") == true
+  }
+
+  @Ignore
+  @Betamax(tape=ChatworkClientSpec.TAPE_NAME, mode = TapeMode.WRITE_ONLY, match = [MatchRule.host, MatchRule.path])
+  def "call ChatWork API and save response to src/test/resources/betamax/tapes/ChatWork_v1_POST_rooms_messages.yaml"(){
+    expect:
+    // TODO: If you want to use, set your actual apiKey and roomId
+    client.sendMessage("testMessage")
+  }
+}
